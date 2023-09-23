bf5f9dbba7c03efe31a446deae04137d9f875a47
SnakeHacKx <omaralexisxdx2@gmail.com>
2023-09-23 12:45:45 -0500

    screens crreadas y barril actualizado

---
+++ lib/presentation/screens/tutorial/app_tutorial_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ApTutorialScreen extends StatelessWidget {
+  static const name = 'tutorial_screen';
+
+  const ApTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
